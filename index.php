30861759688a1d6417d012082afcdfcc5671d586
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-06 17:05:49 +0100

    [edit]: right syntax method in class Genre

--- index.php
+++ index.php
@@ -1,28 +1,46 @@
 <?php
 
+class Genre {
+  public $number;
+
+  public function __construct($_number) {
+    $this-> getGenre($_number);
+  }
+  
+  public function getGenre($_number) {
+    $this->number = $_number;
+    foreach($_number as $key => $value);
+  }
+
+  
+}
+
 class Movie {
   public $name;
   public $genre;
   public $length;
   public $image;
 
-  public function __construct(string $_name, array $_genre, int $_length) {
+  public function __construct(string $_name, Genre $_genre, int $_length) {
     $this->name = $_name;
     $this->length = $_length;
+    $this->genre = $_genre;
   }
 
   public function getGenre() {
     $this->genre = $_genre;
-    if ($_genre == 2) {
-      return "Animazione";
-    } elseif ($_genre == 3) {
-      return "Divertente";
+    foreach($_genre as $key => $value){
+      if ($key == 2) {
+        return "Animazione";
+      }
     }
   }
 
 }
 
-$testMovie1 = new Movie('Il mio amico Totoro', [2, 3], 133);
+
+
+$testMovie1 = new Movie('Il mio amico Totoro', new Genre([2, 3]), 133);
 
 var_dump($testMovie1);
 

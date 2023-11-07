51b3d64af6ae2bf8863269ab8ad55c61457d7add
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-07 15:23:50 +0100

    [edit]: added new properties to Movie instances

--- db/db.php
+++ db/db.php
@@ -1,6 +1,6 @@
 <?
 
 $movies = [
-  new Movie('Il mio amico Totoro', [2, 3], 133, 'mio-vicino-totoro.jpg'),
-  new Movie('Matrix', [1], 198, 'matrix.jpg')
+  new Movie('Il mio amico Totoro', [2, 3], 'mio-vicino-totoro.jpg', 1988, 86),
+  new Movie('Matrix', [1], 'matrix.jpg', 1999, 136)
 ];

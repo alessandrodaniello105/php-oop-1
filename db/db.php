196a30dceb7a12f3ab1d09d747af39a69b4cfa91
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-08 14:39:24 +0100

    [add]: comment to catch errors

--- db/db.php
+++ db/db.php
@@ -11,7 +11,7 @@
 // ];
 
 $productions = [
-    new Movie('Il mio amico Totoro', [2, 3], 'mio-vicino-totoro.jpg', 'asd', 86),
+    new Movie('Il mio amico Totoro', /* try to empty this array to catch error */ [2, 3], 'mio-vicino-totoro.jpg', /* try to give string instead of number to catch error */ 1988, 86),
     new Movie('Matrix', [1], 'matrix.jpg', 1999, 136),
     new TvSerie('Ai confini della realtà', [5, 6, 8], 'twilight-zone.jpg', 1959, 1964, 156, 5),
     new TvSerie('Friends', [11, 14], 'friends.jpg', 1994, 2004, 234, 10),

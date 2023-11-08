35edd17492aff14aa17de640a60ef6dbed779701
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-08 14:35:17 +0100

    [add]: error catch for not numeric published year

--- Model/Movie.php
+++ Model/Movie.php
@@ -5,8 +5,14 @@ class Movie extends Production {
   public $published_year;
   public $running_time;
 
-  public function __construct(string $_name, array $_genreID, string $_image, int $_published_year, int $_running_time) {
-    $this->published_year = $_published_year;
+  public function __construct(string $_name, array $_genreID, string $_image, $_published_year, int $_running_time) {
+    
+    if(is_numeric($_published_year)) {
+      $this->published_year = $_published_year;
+    } else {
+      throw new Exception ("L'anno di pubblicazione del film deve essere un numero");
+    }
+
     $this->running_time = $_running_time;
 
     parent::__construct($_name, $_genreID, $_image);

d56646eb9ad985a9e714da7524b2547bfe5c70da
alessandrodaniello105 <kanymsevement@gmail.com>
2023-11-08 14:34:37 +0100

    [add]: more productions instances for graphic test

--- db/db.php
+++ db/db.php
@@ -11,6 +11,14 @@
 // ];
 
 $productions = [
+    new Movie('Il mio amico Totoro', [2, 3], 'mio-vicino-totoro.jpg', 'asd', 86),
+    new Movie('Matrix', [1], 'matrix.jpg', 1999, 136),
+    new TvSerie('Ai confini della realtà', [5, 6, 8], 'twilight-zone.jpg', 1959, 1964, 156, 5),
+    new TvSerie('Friends', [11, 14], 'friends.jpg', 1994, 2004, 234, 10),
+    new Movie('Il mio amico Totoro', [2, 3], 'mio-vicino-totoro.jpg', 1988, 86),
+    new Movie('Matrix', [1], 'matrix.jpg', 1999, 136),
+    new TvSerie('Ai confini della realtà', [5, 6, 8], 'twilight-zone.jpg', 1959, 1964, 156, 5),
+    new TvSerie('Friends', [11, 14], 'friends.jpg', 1994, 2004, 234, 10),
     new Movie('Il mio amico Totoro', [2, 3], 'mio-vicino-totoro.jpg', 1988, 86),
     new Movie('Matrix', [1], 'matrix.jpg', 1999, 136),
     new TvSerie('Ai confini della realtà', [5, 6, 8], 'twilight-zone.jpg', 1959, 1964, 156, 5),
